0dec79a10876a9fd331ee24e11b4cf5cd8ea18b3
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-02 21:33:27 +1000

    Update test_git_helper.py

--- tests/test_git_helper.py
+++ tests/test_git_helper.py
@@ -1,1 +1,7 @@
 
+from src.git_helper import GitHelper
+from src.repo_initializer import initialize_sample_repo
+def test_git_helper_init():
+    initialize_sample_repo("gh_repo")
+    gh = GitHelper("gh_repo")
+    assert gh.repo is not None
