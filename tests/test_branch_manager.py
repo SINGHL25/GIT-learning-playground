3f027781c91d2d48ff81ea037f494edddcdfe651
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-02 21:32:59 +1000

    Update test_branch_manager.py

--- tests/test_branch_manager.py
+++ tests/test_branch_manager.py
@@ -1,1 +1,9 @@
 
+from src.branch_manager import BranchManager
+from src.repo_initializer import initialize_sample_repo
+def test_branch():
+    initialize_sample_repo("bm_repo")
+    bm = BranchManager("bm_repo")
+    bm.create_branch("test-branch")
+    # no exception = pass
+    assert True
