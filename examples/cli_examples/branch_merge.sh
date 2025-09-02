660890535a000224d97110cddd6a7d16ba629cd2
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-02 21:10:24 +1000

    Update branch_merge.sh

--- examples/cli_examples/branch_merge.sh
+++ examples/cli_examples/branch_merge.sh
@@ -1,1 +1,9 @@
 
+#!/bin/bash
+# Create, switch, merge branch
+git checkout -b feature_branch
+echo "Some changes" >> feature.txt
+git add feature.txt
+git commit -m "Add feature"
+git checkout main
+git merge feature_branch
