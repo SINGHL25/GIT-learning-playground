7ade7302fb6fa23d368e092e2fa53c6df6ad3d1e
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-02 21:33:12 +1000

    Update test_commit_manager.py

--- tests/test_commit_manager.py
+++ tests/test_commit_manager.py
@@ -1,1 +1,8 @@
 
+from src.commit_manager import CommitManager
+from src.repo_initializer import initialize_sample_repo
+def test_commit_list():
+    initialize_sample_repo("cm_repo")
+    cm = CommitManager("cm_repo")
+    commits = cm.list_commits()
+    assert isinstance(commits, list)
