d2f0177f370f896f2b1fbc1e871de577d13b08a7
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-02 21:09:55 +1000

    Update init_clone.sh

--- examples/cli_examples/init_clone.sh
+++ examples/cli_examples/init_clone.sh
@@ -1,1 +1,8 @@
 
+#!/bin/bash
+# Initialize local repo and clone remote repo
+echo "Initializing local repository..."
+git init my_repo
+cd my_repo
+echo "Cloning remote repository..."
+git clone https://github.com/username/sample_repo.git
